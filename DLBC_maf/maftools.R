bd9017455e21b8f49d55413acc44b493834feee2
Epitácio Farias <fariasepitacio@ufrn.edu.br>
2020-08-26 21:38:09 -0300

    Addition of the mutational signature code
    Has been additionated the code of mutational signature, but I saw error's in survival analysis  (Lines 157 to 164)  and in the graphic of "Differences between APOBEC enriched and non-enriched samples "(Line 220). Using "HGVSp_Short" in the fields "AACol" the graphs have appeared but not with the most mutated gene's.

--- DLBC_maf/maftools.R
+++ DLBC_maf/maftools.R
@@ -7,8 +7,8 @@
 
 
 ## Installing packages
-packages_bioconductor <- c("TCGAbiolinks","maftools","BSgenome.Hsapiens.UCSC.hg38")
-packages_cran <- c("tidyverse", "pheatmap")
+packages_bioconductor <- c("TCGAbiolinks","maftools","BSgenome.Hsapiens.UCSC.hg38","SummarizedExperiment")
+packages_cran <- c("DT", "tidyverse", "stringr", "data.table", "pheatmap","NMF")
 
 #use this function to check if each package is on the local machine
 #if a package is installed, it will be loaded
@@ -79,8 +79,8 @@ mafDlbc.titv <- titv(maf = dlbc.maf, plot = FALSE, useSyn = TRUE)
 plotTiTv(res = mafDlbc.titv)
 
 # lollipop plot for IGHV2-70, which is one of the most frequent mutated gene in DLBC
-lollipopPlot(maf = dlbc.maf, gene = 'CARD11', AACol = 'Amino_acids', showMutationRate = TRUE)
-lollipopPlot(maf = dlbc.mafclin, gene = 'MUC16', AACol = 'Amino_acids', showDomainLabel = FALSE)
+lollipopPlot(maf = dlbc.maf, gene = 'CARD11', AACol = 'HGVSp_Short', showMutationRate = TRUE)
+lollipopPlot(maf = dlbc.mafclin, gene = 'BTG2', AACol = 'HGVSp_Short', showDomainLabel = FALSE)
 
 # rainfall plots highlights hyper-mutated genomic regions
 # Kataegis: genomic segments containing six or more consecutive mutations with an average inter-mutation distance of less than or equal to 1,00 bp
@@ -112,11 +112,11 @@ somaticInteractions(maf = dlbc.maf, top = 10, pvalue = c(0.05, 0.1))
 
 # detecting cancer driver genes based on positional clustering
 # most of the variants in cancer causing genes are enriched at few specific loci (aka hot-spots)
-dlbc.sig <- oncodrive(maf = dlbc.maf, AACol = 'AA_MAF', minMut = 5, pvalMethod = 'zscore')
-plotOncodrive(res = dlbcl.sig, fdrCutOff = 0.1, useFraction = TRUE) # AACol = 'Amino_acids'?
+dlbc.sig <- oncodrive(maf = dlbc.maf, AACol = 'HGVSp_Short', minMut = 5, pvalMethod = 'zscore')
+plotOncodrive(res = dlbc.sig, fdrCutOff = 0.1, useFraction = TRUE) # AACol = 'Amino_acids'?
 
 # pfam domain
-dlbc.pfam = pfamDomains(maf = dlbc.maf, AACol = 'AA_MAF', top = 10) # idem
+dlbc.pfam = pfamDomains(maf = dlbc.maf, AACol = 'HGVSp_Short', top = 10) # idem
 
 
 ## Reading gistic or CNV -------------------------
@@ -157,10 +157,10 @@ gisticOncoPlot(gistic = dlbc.gistic, top = 20)
 ## Survival Analysis ------------------------------
 
 # it requires input data with Tumor_Sample_Barcode, binary event (1/0) and time to event.
-mafSurvival(maf = dlbc.mafclin, genes = 'IGHV2-70', time = 'time', Status = 'vital_status', isTCGA = TRUE)
+mafSurvival(maf = dlbc.mafclin, genes = 'IGHV2-70', time = 'time', Status = 'vital_status', isTCGA = FALSE)
 
 # identify a set of genes (of size 2) to predict poor prognostic groups
-prog_geneset <- survGroup(maf = dlbc.mafclin, top = 20, geneSetSize = 2, time = "days_to_last_follow_up", Status = "vital_status", verbose = FALSE)
+prog_geneset <- survGroup(maf = dlbc.mafclin, top = 20, geneSetSize = 2, time = "time", Status = "vital_status", verbose = FALSE)
 
 
 ## Clinical enrichment analysis ------------------
@@ -188,10 +188,92 @@ PlotOncogenicPathways(maf = dlbc.maf, pathways = "NOTCH")
 
 
 ## Mutational signatures --------------------------
-# signatures can be extracted by decomposing matrix of nucleotide substitutions and compared to validated signatures
-# < code here >
+#Download Mutational Data
 
+dlbc.mutect.maf <- GDCquery_Maf("DLBC", pipelines = "mutect2")
 
+# Number of mutations on muse:  6094
+#dim(DLBC.muse.maf)[1]
+# Number of mutations on mutect2:  6406
+#dim(DLBC.mutect.maf)[1]
+# Number of mutations on varscan2:  6280
+#dim(DLBC.varscan2.maf)[1]
+# Number of mutations on somaticsniper:  4969
+#dim(DLBC.somaticsniper.maf)[1]
+# Number of Patients: 37
+#length(unique(substr(DLBC.mutect.maf$Tumor_Sample_Barcode,1,12)))
+
+#Requires BSgenome object
+library(BSgenome.Hsapiens.UCSC.hg38, quietly = TRUE)
+
+dlbc.mutect.maf_clin <- read.maf(maf = dlbc.mutect.maf, 
+                                 clinicalData=clinical, 
+                                 verbose = T, 
+                                 isTCGA = T, 
+                                 removeDuplicatedVariants = F)
+
+#Trinucleotide Matrix
+dlbc.tnm = trinucleotideMatrix(maf = dlbc.mutect.maf_clin, prefix = '', add = TRUE,
+                               ref_genome = "BSgenome.Hsapiens.UCSC.hg38")
+
+#APOBEC Differentiation by Trinucleotide Matrix
+plotApobecDiff(tnm = dlbc.tnm, maf = dlbc.mutect.maf_clin, pVal = 0.05)
+
+#plotSignatures - plots signatures
+par(mar = c(2, 2, 2, 1))
+plot(NA, xlim = c(1, 10), ylim = c(0, 30), frame.plot = FALSE, axes = FALSE, xlab = NA, ylab = NA)
+rect(xleft = 3, ybottom = 28, xright = 7, ytop = 30, col = grDevices::adjustcolor("gray70", alpha.f = 0.6), lwd = 1.2, border = "maroon")
+text(x = 5, y = 29, labels = "MAF", font = 2)
+arrows(x0 = 5, y0 = 28, x1 = 5, y1 = 26, length = 0.1, lwd = 2)
+text(x = 5, y = 25, labels = "trinucleotideMatrix()", font = 3)
+arrows(x0 = 5, y0 = 24, x1 = 5, y1 = 21, length = 0.1, lwd = 2)
+text(x = 5, y = 20, labels = "estimateSignatures()", font = 3)
+arrows(x0 = 5, y0 = 19, x1 = 5, y1 = 16, length = 0.1, lwd = 2)
+text(x = 5, y = 15, labels = "plotCophenetic()", font = 3)
+arrows(x0 = 5, y0 = 14, x1 = 5, y1 = 11, length = 0.1, lwd = 2)
+text(x = 5, y = 10, labels = "extractSignatures()", font = 3)
+arrows(x0 = 5, y0 = 9, x1 = 5, y1 = 6, length = 0.1, lwd = 2)
+text(x = 5, y = 5, labels = "compareSignatures()", font = 3)
+arrows(x0 = 5, y0 = 4, x1 = 5, y1 = 1, length = 0.1, lwd = 2)
+text(x = 5, y = 0, labels = "plotSignatures()", font = 3)
+
+#Run main function with maximum 10 signatures. 
+
+library('NMF')
+dlbc.sign = estimateSignatures(mat = dlbc.tnm, nTry = 10, pConstant = 0.1, plotBestFitRes = T, parallel = 2)
+
+# Analysis with 4 gene signatures
+dlbc.sig = extractSignatures(mat = dlbc.tnm, n = 4, pConstant = 0.1,  parallel = 2)
+
+#Compate against original 30 signatures 
+dlbc.og30.cosm = compareSignatures(nmfRes = dlbc.sig, sig_db = "legacy")
+
+#library('pheatmap')
+pheatmap::pheatmap(mat = dlbc.og30.cosm$cosine_similarities, 
+                   cluster_rows = FALSE, 
+                   angle_col = "45",
+                   cellwidth = 20, cellheight = 20,
+                   width = 7, height=4,
+                   main = "Cosine similarity against validated signatures - Legacy")
+
+#Compate against updated version3 60 signatures 
+dlbc.v4.cosm = compareSignatures(nmfRes = dlbc.sig, sig_db = "SBS")
+
+#library('pheatmap')
+pheatmap::pheatmap(mat = dlbc.v4.cosm$cosine_similarities, 
+                   cluster_rows = FALSE, 
+                   angle_col = "45",
+                   cellwidth = 20, cellheight = 20,
+                   width = 7, height=4,                   
+                   main = "Cosine similarity against validated signatures - SBS")
+
+maftools::plotSignatures(nmfRes = dlbc.sig, title_size = 0.9, sig_db = "legacy")
+
+maftools::plotSignatures(nmfRes = dlbc.sig, title_size = 0.9, sig_db = "SBS")
+
+dlbc.se = signatureEnrichment(maf = dlbc.mutect.maf_clin, sig_res = dlbc.sig)
+
+plotEnrichmentResults(enrich_res = dlbc.se, pVal = 0.05)
 -----------------------------------------
 -----------------------------------------
      
