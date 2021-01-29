642c0ae9f3020a485db3546d5428b7e43b124576
Epitácio Farias <fariasepitacio@ufrn.edu.br>
2021-01-29 15:24:57 -0300

    gene_signature
    Initial study aboult gene signature of the DLBCL

---
+++ gene_signature/data/download_data_cbioportal.R
@@ -0,0 +1,22 @@
+url <- "https://media.githubusercontent.com/media/cBioPortal/datahub/master/public/dlbc_tcga_pan_can_atlas_2018/data_RNA_Seq_v2_expression_median.txt"
+destfile <- "~/GitHub/Project_Lymphoma_DLBC/gene_signature/data/dlbc_exp.txt"
+download.file(url, destfile)
+
+
+url <- "https://media.githubusercontent.com/media/cBioPortal/datahub/master/public/dlbc_tcga_pan_can_atlas_2018/data_clinical_patient.txt"
+destfile <- "~/GitHub/Project_Lymphoma_DLBC/gene_signature/data/data_clinical.txt"
+download.file(url, destfile)
+
+
+data_clinical <- read.delim("~/GitHub/Project_Lymphoma_DLBC/gene_signature/data/data_clinical.txt", comment.char="#", na.strings=c("NA", ""))
+
+dlbc_exp <- read.delim("~/GitHub/Project_Lymphoma_DLBC/gene_signature/data/dlbc_exp.txt", comment.char="#", na.strings=c("NA", ""))
+
+dlbc_exp <- dlbc_exp[!is.na(dlbc_exp$Hugo_Symbol),]
+dlbc_exp <- dlbc_exp[!duplicated(dlbc_exp$Hugo_Symbol),]
+rownames(dlbc_exp) <- as.character(dlbc_exp$Hugo_Symbol)
+dlbc_exp$Entrez_Gene_Id <- NULL
+dlbc_exp$Hugo_Symbol <- NULL
+
+
+data_clinical
