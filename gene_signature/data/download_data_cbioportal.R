913c498cad471e8eb57a0bca81f4c86f518a5398
terrematte <ptrckphilo@gmail.com>
2021-02-02 15:13:05 -0300

    Correcting the analysis of Differential expression of DLBC.

--- gene_signature/data/download_data_cbioportal.R
+++ gene_signature/data/download_data_cbioportal.R
@@ -1,16 +1,16 @@
 url <- "https://media.githubusercontent.com/media/cBioPortal/datahub/master/public/dlbc_tcga_pan_can_atlas_2018/data_RNA_Seq_v2_expression_median.txt"
-destfile <- "~/GitHub/Project_Lymphoma_DLBC/gene_signature/data/dlbc_exp.txt"
+destfile <- "data/dlbc_exp.txt"
 download.file(url, destfile)
 
 
 url <- "https://media.githubusercontent.com/media/cBioPortal/datahub/master/public/dlbc_tcga_pan_can_atlas_2018/data_clinical_patient.txt"
-destfile <- "~/GitHub/Project_Lymphoma_DLBC/gene_signature/data/data_clinical.txt"
+destfile <- "data/data_clinical.txt"
 download.file(url, destfile)
 
 
-data_clinical <- read.delim("~/GitHub/Project_Lymphoma_DLBC/gene_signature/data/data_clinical.txt", comment.char="#", na.strings=c("NA", ""))
+data_clinical <- read.delim("data/data_clinical.txt", comment.char="#", na.strings=c("NA", ""))
 
-dlbc_exp <- read.delim("~/GitHub/Project_Lymphoma_DLBC/gene_signature/data/dlbc_exp.txt", comment.char="#", na.strings=c("NA", ""))
+dlbc_exp <- read.delim("data/dlbc_exp.txt", comment.char="#", na.strings=c("NA", ""))
 
 dlbc_exp <- dlbc_exp[!is.na(dlbc_exp$Hugo_Symbol),]
 dlbc_exp <- dlbc_exp[!duplicated(dlbc_exp$Hugo_Symbol),]
@@ -19,4 +19,4 @@ dlbc_exp$Entrez_Gene_Id <- NULL
 dlbc_exp$Hugo_Symbol <- NULL
 
 
-data_clinical
+#data_clinical
